69fe5c64b9d7132dc7dedbb49b9237c5a1eb6290
Mustafa Caglar <mc934@cam.ac.uk>
2018-05-21 12:12:50 -0700

    Update

--- matlabstats/ModelFit/LorentizianFit.m
+++ matlabstats/ModelFit/LorentizianFit.m
@@ -19,15 +19,15 @@ area = ParamFinal(:,3);
 
 Y_fit = Lorentzian(X,location,scale, area);
 
-<<<<<<< mine
+%<<<<<<< mine
 hold on
 plot(X,Y_fit,'b')
 hold off
-=======
+%=======
  %hold on
  %plot(X,Lorentzian(X,location,scale, area),'b')
 % hold off
->>>>>>> theirs
+%>>>>>>> theirs
 
 FinalResids = LorentzianSqaureResids(X,Y,ParamFinal);
 
